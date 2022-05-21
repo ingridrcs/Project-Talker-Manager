9f7d007d186f80f0ccd07f7f7f7b7b863342a298
Ingrid_Ramos <ingridrcs@gmail.com>
2022-05-20 21:48:16 -0300

    fix req 3

--- middleware/validateTask.js
+++ middleware/validateTask.js
@@ -4,14 +4,14 @@ const validationLogin = (req, res, next) => {
  if (!email) {
    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
  }
- if (email !== validEmail) {
+ if (!email.matches(validEmail)) {
    return res.status(400).json({ message: 'O "email" deve ter o formato \'email@email.com\'' });
  }
  if (!password) {
-   return res.status(200).json({ message: 'O campo "password" é obrigatório' });
+   return res.status(400).json({ message: 'O campo "password" é obrigatório' });
  }
   if (password.length < 6) {
-   return res.status(200).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+   return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
  }
  next();
 }; 
